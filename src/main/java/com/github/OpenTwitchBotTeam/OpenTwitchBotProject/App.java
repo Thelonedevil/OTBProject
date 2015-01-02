d36440ebe2eb76d28faf28805c7d4322eb67a60e
Justin Wiblin <Justin_wiblin@hotmail.co.uk>
2015-01-02 02:26:42 +0000

    Fixed project structure
    Signed-off-by: Justin Wiblin <Justin_wiblin@hotmail.co.uk>

--- src/main/java/com/github/OpenTwitchBotTeam/OpenTwitchBotProject/App.java
+++ src/main/java/com/github/OpenTwitchBotTeam/OpenTwitchBotProject/App.java
@@ -1,8 +1,10 @@
+package com.github.OpenTwitchBotTeam.OpenTwitchBotProject;
+
 /**
  * Created by justin on 02/01/2015.
  */
 public class App {
-    
+
     public static void main(String[] args) {
 
     }
