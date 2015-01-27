701df1d7f6d9ff0b9786615c6617341116538f0a
NthPortal <mail.vanpelt@gmail.com>
2015-01-26 19:10:52 -0500

    Changed parser to allow spaces in defaults, and to add more terms.

--- src/main/java/com/github/opentwitchbotteam/otbproject/commands/parser/CommandResponseParser.java
+++ src/main/java/com/github/opentwitchbotteam/otbproject/commands/parser/CommandResponseParser.java
@@ -10,84 +10,104 @@ public class CommandResponseParser {
     private static final String DEFAULT_END = "\\}\\}";   //regex
 
 
-    public static String parse(String userNick, int count, String[] args, String rawResponse) {
-        String[] rawResponseArray = rawResponse.split(" ");
-        ArrayList<String> formattedResponseArray = new ArrayList<String>();
-
-        for (String word : rawResponseArray) {
-            formattedResponseArray.add(parseWord(userNick, count, args, word));
-        }
-        return String.join(" ", formattedResponseArray);
-    }
-
-    private static String parseWord(String userNick, int count, String[] args, String word) {
+    public static String parse(String userNick, String channel, int count, String[] args, String rawResponse) {
         int innerStartIndex;
         int innerEndIndex;
         String temp;
 
         while (true) {
             // If no more terms, return
-            innerStartIndex = word.lastIndexOf(TERM_START);
+            innerStartIndex = rawResponse.lastIndexOf(TERM_START);
             if (innerStartIndex == -1) {
-                return word;
+                return rawResponse;
             }
             // If unbalanced term delimiters, return
-            innerEndIndex = word.indexOf(TERM_END, innerStartIndex);
+            innerEndIndex = rawResponse.indexOf(TERM_END, innerStartIndex);
             if (innerEndIndex == -1) {
-                return word;
+                return rawResponse;
             }
 
             // Check if valid term, and replace if so
             try {
-                temp = parseTerm(userNick, count, args, word.substring(innerStartIndex + 2, innerEndIndex));
+                temp = parseTerm(userNick, channel, count, args, rawResponse.substring(innerStartIndex + 2, innerEndIndex));
             }
             catch (InvalidTermException e) {
-                return word;
+                return rawResponse;
             }
-            word = word.substring(0, innerStartIndex) + temp + word.substring(innerEndIndex + 2);
+            rawResponse = rawResponse.substring(0, innerStartIndex) + temp + rawResponse.substring(innerEndIndex + 2);
         }
     }
 
-    private static String parseTerm(String userNick, int count, String[] args, String word) throws InvalidTermException {
+    private static String parseTerm(String userNick, String channel, int count, String[] args, String term) throws InvalidTermException {
         // [[user.modifier]]
-        if (word.matches("^user(" + MODIFIER_DELIM + "\\p{Alpha}*)?$")) {
-            return doModifier(userNick, word);
+        if (term.matches("^user(" + MODIFIER_DELIM + "\\p{Alpha}*)?$")) {
+            return doModifier(userNick, term);
+        }
+        // [[channel.modifier]]
+        if (term.matches("^channel(" + MODIFIER_DELIM + "\\p{Alpha}*)?$")) {
+            return doModifier(channel, term);
         }
         // [[count]] - ignores modifier (because no effect)
-        else if (word.matches("^count(" + MODIFIER_DELIM + "\\p{Alpha}*)?$")) {
+        else if (term.matches("^count(" + MODIFIER_DELIM + "\\p{Alpha}*)?$")) {
             return Integer.toString(count);
         }
         // [[quote.modifier]] - can have a modifier, but it's unclear why you want one
-        else if (word.matches("^quote(" + MODIFIER_DELIM + "\\p{Alpha}*)?$")) {
+        else if (term.matches("^quote(" + MODIFIER_DELIM + "\\p{Alpha}*)?$")) {
             return "[Quotes are not yet implemented]"; // TODO fix when quotes implemented
         }
         // [[game.modifier]]
-        else if (word.matches("^game(" + MODIFIER_DELIM + "\\p{Alpha}*)?$")) {
+        else if (term.matches("^game(" + MODIFIER_DELIM + "\\p{Alpha}*)?$")) {
             return "a game"; // TODO fix when able to get game name from twitch
         }
         // [[args.modifier{{default}}]]
-        else if (word.matches("^args(" + MODIFIER_DELIM + "\\p{Alpha}*)?(" + DEFAULT_START + ".*" + DEFAULT_END + ")?$")) {
+        else if (term.matches("^args(" + MODIFIER_DELIM + "\\p{Alpha}*)?(" + DEFAULT_START + ".*" + DEFAULT_END + ")?$")) {
             // If no args, parse default
             if (args.length == 0) {
-                return getDefault(word);
+                return getDefault(term);
             }
-            return doModifier(String.join(" ", args), word);
+            return doModifier(String.join(" ", args), term);
+        }
+        // [[ifargs{{string}}]] - ignores modifier
+        else if (term.matches("^ifargs(" + MODIFIER_DELIM + "\\p{Alpha}*)?(" + DEFAULT_START + ".*" + DEFAULT_END + ")?$")) {
+            // If no args, return empty string
+            if (args.length == 0) {
+                return "";
+            }
+            // Gets conditionally printed string
+            return getDefault(term);
         }
         // [[argN.modifier{{default}}]] - N is a natural number
-        else if (word.matches("^arg\\p{Digit}+(" + MODIFIER_DELIM + "\\p{Alpha}*)?(" + DEFAULT_START + ".*" + DEFAULT_END + ")?$")) {
+        else if (term.matches("^arg\\p{Digit}+(" + MODIFIER_DELIM + "\\p{Alpha}*)?(" + DEFAULT_START + ".*" + DEFAULT_END + ")?$")) {
             // Gets arg number
-            String argNumStr = word.replaceFirst("arg", "").split(DEFAULT_START, 2)[0].split(MODIFIER_DELIM, 2)[0];
+            String argNumStr = term.replaceFirst("arg", "").split(DEFAULT_START, 2)[0].split(MODIFIER_DELIM, 2)[0];
             int argNum = Integer.parseInt(argNumStr);
 
-            // If argNum is 0, invalid term
-            if (argNum == 0) {
+            // If argNum is 0 (or max int overflow), invalid term
+            if (argNum <= 0) {
                 throw new InvalidTermException();
             }
             // If insufficient args, parse default
             if (args.length < argNum) {
-                return getDefault(word);
+                return getDefault(term);
             }
-            return doModifier(args[argNum], word);
+            return doModifier(args[argNum], term);
+        }
+        // [[ifargN{{string}}]] - N is a natural number; ignores modifier
+        else if (term.matches("^ifarg\\p{Digit}+(" + MODIFIER_DELIM + "\\p{Alpha}*)?(" + DEFAULT_START + ".*" + DEFAULT_END + ")?$")) {
+            // Gets arg number
+            String argNumStr = term.replaceFirst("ifarg", "").split(DEFAULT_START, 2)[0].split(MODIFIER_DELIM, 2)[0];
+            int argNum = Integer.parseInt(argNumStr);
+
+            // If argNum is 0 (or max int overflow)-, invalid term
+            if (argNum <= 0) {
+                throw new InvalidTermException();
+            }
+            // If insufficient args, return empty string
+            if (args.length < argNum) {
+                return "";
+            }
+            // Gets conditionally printed string
+            return getDefault(term);
         }
 
         else {
