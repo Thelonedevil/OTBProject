73f2061680a0d4aefb6140db4a1b0868f63a34e3
Justin Wiblin <Justin_wiblin@hotmail.co.uk>
2015-01-29 22:37:50 +0000

    Added basic bot code
    Signed-off-by: Justin Wiblin <Justin_wiblin@hotmail.co.uk>

--- src/main/java/com/github/opentwitchbotteam/otbproject/App.java
+++ src/main/java/com/github/opentwitchbotteam/otbproject/App.java
@@ -1,11 +1,34 @@
 package com.github.opentwitchbotteam.otbproject;
 
+import org.pircbotx.Configuration;
+import org.pircbotx.PircBotX;
+import org.pircbotx.exception.IrcException;
+import org.pircbotx.hooks.Listener;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by justin on 02/01/2015.
  */
 public class App {
-
+    static HashSet<String> channels = new HashSet<>();
+    static Listener listener;
+    static PircBotX bot;
     public static void main(String[] args) {
-
+        Configuration.Builder configurationBuilder = new Configuration.Builder().setName("Lone_Bot").setAutoNickChange(false).setCapEnabled(false).addListener(listener).setServerHostname("irc.twitch.tv")
+                .setServerPort(6667).setServerPassword("").setEncoding(Charset.forName("UTF-8"));
+        channels.forEach(configurationBuilder::addAutoJoinChannel);
+        Configuration configuration = configurationBuilder.buildConfiguration();
+        bot = new PircBotX(configuration);
+        try {
+            bot.startBot();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (IrcException e) {
+            e.printStackTrace();
+        }
     }
 }
