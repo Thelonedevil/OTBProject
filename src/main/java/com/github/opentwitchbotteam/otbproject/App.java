b8ed2585edaa1b0d972a07fac2b432a27a3d2353
Justin Wiblin <Justin_wiblin@hotmail.co.uk>
2015-01-29 22:49:45 +0000

    Added basic irc listener framework
    Signed-off-by: Justin Wiblin <Justin_wiblin@hotmail.co.uk>

--- src/main/java/com/github/opentwitchbotteam/otbproject/App.java
+++ src/main/java/com/github/opentwitchbotteam/otbproject/App.java
@@ -1,5 +1,6 @@
 package com.github.opentwitchbotteam.otbproject;
 
+import com.github.opentwitchbotteam.otbproject.eventlistener.IrcListener;
 import org.pircbotx.Configuration;
 import org.pircbotx.PircBotX;
 import org.pircbotx.exception.IrcException;
@@ -8,14 +9,13 @@ import org.pircbotx.hooks.Listener;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.HashSet;
-import java.util.Set;
 
 /**
  * Created by justin on 02/01/2015.
  */
 public class App {
     static HashSet<String> channels = new HashSet<>();
-    static Listener listener;
+    static Listener listener = new IrcListener();
     static PircBotX bot;
     public static void main(String[] args) {
         Configuration.Builder configurationBuilder = new Configuration.Builder().setName("Lone_Bot").setAutoNickChange(false).setCapEnabled(false).addListener(listener).setServerHostname("irc.twitch.tv")
