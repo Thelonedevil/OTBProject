3c72d65eac7669eac4ccfae5edb5d643e81c71dc
Justin Wiblin <Justin_wiblin@hotmail.co.uk>
2015-01-30 02:24:28 +0000

    Added start of the logging framework, rerouting PircBotX's logging though Log4J2 for a consistent output, and storage. Every start up should backup the previous log file, log files go into .logs/ right now. Easily changeable.
    Signed-off-by: Justin Wiblin <Justin_wiblin@hotmail.co.uk>

--- src/main/java/com/github/opentwitchbotteam/otbproject/App.java
+++ src/main/java/com/github/opentwitchbotteam/otbproject/App.java
@@ -1,13 +1,18 @@
 package com.github.opentwitchbotteam.otbproject;
 
 import com.github.opentwitchbotteam.otbproject.eventlistener.IrcListener;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.pircbotx.Configuration;
 import org.pircbotx.PircBotX;
 import org.pircbotx.exception.IrcException;
 import org.pircbotx.hooks.Listener;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashSet;
 
 /**
@@ -17,16 +22,19 @@ public class App {
     public static HashSet<String> channels = new HashSet<>();
     static Listener listener = new IrcListener();
     public static PircBotX bot;
+    public static final Logger logger = LogManager.getLogger();
     public static void main(String[] args) {
+
         Configuration.Builder configurationBuilder = new Configuration.Builder().setName("Lone_Bot").setAutoNickChange(false).setCapEnabled(false).addListener(listener).setServerHostname("irc.twitch.tv")
                 .setServerPort(6667).setServerPassword("").setEncoding(Charset.forName("UTF-8"));
         channels.forEach(configurationBuilder::addAutoJoinChannel);
         Configuration configuration = configurationBuilder.buildConfiguration();
         bot = new PircBotX(configuration);
         try {
+            logger.info("Bot Started");
             bot.startBot();
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.catching(e);
         } catch (IrcException e) {
             e.printStackTrace();
         }
