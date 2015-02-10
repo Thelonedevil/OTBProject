5afc586f720341a7af852bd27518c9b0b08cd413
NthPortal <mail.vanpelt@gmail.com>
2015-02-09 21:54:28 -0500

    Added CommandLoader class. Attempted to load example command into database, but the DatabaseWrapper threw an SQL exception.

--- src/main/java/com/github/otbproject/otbproject/util/dev/DevHelper.java
+++ src/main/java/com/github/otbproject/otbproject/util/dev/DevHelper.java
@@ -1,10 +1,20 @@
 package com.github.otbproject.otbproject.util.dev;
 
+import com.github.otbproject.otbproject.App;
+import com.github.otbproject.otbproject.commands.Command;
+import com.github.otbproject.otbproject.commands.loader.CommandLoader;
+import com.github.otbproject.otbproject.commands.loader.LoadedCommand;
+import com.github.otbproject.otbproject.database.DatabaseHelper;
+import com.github.otbproject.otbproject.database.DatabaseWrapper;
+import com.github.otbproject.otbproject.fs.FSUtil;
 import com.github.otbproject.otbproject.fs.Setup;
 import com.github.otbproject.otbproject.util.CommandFileGenerator;
 import com.github.otbproject.otbproject.util.ConfigFileGenerator;
+import com.github.otbproject.otbproject.util.JsonHandler;
 
+import java.io.File;
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class DevHelper {
     public static void run(String[] args) {
@@ -17,7 +27,18 @@ public class DevHelper {
         }
 
         //generateConfigFiles();
-        generateCommandFiles();
+        //generateCommandFiles();
+
+        // Test command loading
+        LoadedCommand command = JsonHandler.readValue(FSUtil.defaultsDir() + File.separator + "example-command.json", LoadedCommand.class);
+        DatabaseWrapper db = DatabaseHelper.getChannelDatabase("the_lone_devil");
+        CommandLoader.addCommandFromLoadedCommand(db, command);
+        App.logger.info("Command was successfully loaded:");
+        try {
+            App.logger.info(Command.exists(db, "!example-command-name"));
+        } catch (SQLException e) {
+            App.logger.catching(e);
+        }
     }
 
     private static void generateConfigFiles() {
