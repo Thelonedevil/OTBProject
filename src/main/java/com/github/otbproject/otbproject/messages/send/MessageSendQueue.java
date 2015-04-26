137b549bee039d03cf07221781a2f51318d4f974
NthPortal <mail.vanpelt@gmail.com>
2015-04-25 23:46:41 -0400

    Revert "Changed MessageSendQueue to remove messages ofa given priority if the queue passes the priority size limits of the message."
    This reverts commit 109d4686593204c921477981f5996854d68029d0.

--- src/main/java/com/github/otbproject/otbproject/messages/send/MessageSendQueue.java
+++ src/main/java/com/github/otbproject/otbproject/messages/send/MessageSendQueue.java
@@ -1,14 +1,11 @@
 package com.github.otbproject.otbproject.messages.send;
 
 import com.github.otbproject.otbproject.channels.Channel;
-import com.google.common.collect.MinMaxPriorityQueue;
 
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.PriorityBlockingQueue;
 
 public class MessageSendQueue {
-    private final MinMaxPriorityQueue<MessageOut> deque = MinMaxPriorityQueue.create();
-    private final ReadWriteLock lock = new ReentrantReadWriteLock();
+    private final PriorityBlockingQueue<MessageOut> queue = new PriorityBlockingQueue<>();
     private final Channel channel;
 
     public MessageSendQueue(Channel channel) {
@@ -16,12 +13,7 @@ public class MessageSendQueue {
     }
 
     public MessageOut take() throws InterruptedException {
-        lock.writeLock().lock();
-        try {
-            return deque.removeFirst();
-        } finally {
-            lock.writeLock().unlock();
-        }
+        return queue.take();
     }
 
     public boolean add(MessageOut message) {
@@ -30,58 +22,6 @@ public class MessageSendQueue {
         }
 
         MessagePriority priority = message.getPriority();
-        int limit = getPriorityLimit(priority);
-
-        if ((limit >= 0) && deque.size() > limit) {
-            return false;
-        }
-
-        lock.writeLock().lock();
-        try {
-            deque.add(message);
-            prune();
-        } finally {
-            lock.writeLock().unlock();
-        }
-        return true;
-    }
-
-    public void clear() {
-        lock.writeLock().lock();
-        try {
-            deque.clear();
-        } finally {
-            lock.writeLock().unlock();
-        }
-    }
-
-    public int size() {
-        lock.readLock().lock();
-        try {
-            return deque.size();
-        } finally {
-            lock.readLock().unlock();
-        }
-    }
-
-    // MUST CALL FROM WITHIN LOCKED CODE BLOCK
-    private void prune() {
-        int limit;
-
-        while (true) {
-            // Get limit of last element
-            limit = getPriorityLimit(deque.peekLast().getPriority());
-
-            // Remove last element if size is larger than limit for that element
-            if ((limit >=0) && deque.size() > limit) {
-                deque.removeLast();
-            } else {
-                break;
-            }
-        }
-    }
-
-    private int getPriorityLimit(MessagePriority priority) {
         // Defaults to no limit
         int limit = -1;
 
@@ -93,6 +33,18 @@ public class MessageSendQueue {
             limit = channel.getConfig().queueLimits.getLowPriorityLimit();
         }
 
-        return limit;
+        if ((limit >= 0) && queue.size() > limit) {
+            return false;
+        }
+
+        return queue.add(message);
+    }
+
+    public void clear() {
+        queue.clear();
+    }
+
+    public int size() {
+        return queue.size();
     }
 }
