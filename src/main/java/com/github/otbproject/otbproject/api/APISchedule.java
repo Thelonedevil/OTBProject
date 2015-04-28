b7603d85464a9bb23e20297b895231000b3897dc
NthPortal <mail.vanpelt@gmail.com>
2015-04-28 18:53:57 -0400

    Wrote algorithm for getting correct offset. Rearranged some stuff when doing so.

--- src/main/java/com/github/otbproject/otbproject/api/APISchedule.java
+++ src/main/java/com/github/otbproject/otbproject/api/APISchedule.java
@@ -1,6 +1,7 @@
 package com.github.otbproject.otbproject.api;
 
 import com.github.otbproject.otbproject.App;
+import com.github.otbproject.otbproject.channels.Channel;
 import com.github.otbproject.otbproject.commands.scheduler.ResetTask;
 import com.github.otbproject.otbproject.commands.scheduler.ScheduledCommand;
 import com.github.otbproject.otbproject.commands.scheduler.SchedulerFields;
@@ -16,13 +17,15 @@ import java.util.concurrent.TimeUnit;
  * Created by Justin on 20/04/2015.
  */
 public class APISchedule {
+    public static final int SECONDS_IN_MINUTE = 60;
+    public static final int SECONDS_IN_HOUR = 360;
 
     public static void scheduleCommandInSeconds(String channel, String command, long delay, long period, boolean hourReset) {
         scheduleCommand(channel, command, delay, period, hourReset, TimeUnit.SECONDS);
     }
 
     public static void scheduleCommandInMinutes(String channel, String command, long delay, long period, boolean hourReset) {
-        scheduleCommand(channel, command, delay, period, hourReset, TimeUnit.MINUTES);
+        scheduleCommand(channel, command, (delay * SECONDS_IN_MINUTE), (period * SECONDS_IN_MINUTE), hourReset, TimeUnit.SECONDS);
     }
 
     public static void scheduleCommandInHours(String channel, String command, long delay, long period) {
@@ -35,22 +38,45 @@ public class APISchedule {
             APIChannel.get(channel).getHourlyResetSchedules().get(command).cancel(false);
     }
 
-    public static int getMinutesTillTheHour() {
+    public static int getSecondsSinceTheHour() {
         Date date = new Date();
         Calendar calendar = GregorianCalendar.getInstance();
         calendar.setTime(date);
-        return 60 - calendar.get(Calendar.MINUTE);
+        return calendar.get(Calendar.SECOND);
+    }
+
+    public static int getSecondsTillTheHour() {
+        return SECONDS_IN_HOUR - getSecondsSinceTheHour();
     }
 
     private static boolean scheduleCommand(String channel, String command, long delay, long period, boolean hourReset, TimeUnit timeUnit) {
         Runnable task = new ScheduledCommand(channel, command);
-        APIChannel.get(channel).getScheduledCommands().put(command, APIChannel.get(channel).getScheduler().schedule(task, getMinutesTillTheHour() + delay, period, timeUnit));
-        App.logger.debug("Scheduled Command: " + command + " to run every " + period + " " + timeUnit.toString());
-        if (hourReset) {
+        if (!hourReset) {
+            scheduleCommand(channel, command, task, delay, period, timeUnit);
+        } else {
+            long result = getSecondsSinceTheHour() - delay;
+            if (result < 0) {
+                scheduleCommand(channel, command, task, (-1 * result), period, timeUnit);
+            } else {
+                // This gets the correct delay. Trust me
+                scheduleCommand(channel, command, task, (period - (result % period)), period, timeUnit);
+            }
+            // Setup reset
             Runnable reset = new ResetTask(channel, command, delay, period, timeUnit);
-            APIChannel.get(channel).getHourlyResetSchedules().put(command, APIChannel.get(channel).getScheduler().schedule(reset, getMinutesTillTheHour(), 1, TimeUnit.HOURS));
+            APIChannel.get(channel).getHourlyResetSchedules().put(command, APIChannel.get(channel).getScheduler().schedule(reset, getSecondsTillTheHour(), SECONDS_IN_HOUR, TimeUnit.SECONDS));
         }
         return addToDatabase(channel, command, delay, period, hourReset, timeUnit);
+    }
+
+    private static boolean scheduleCommand(String channelName, String command, Runnable task, long delay, long period, TimeUnit timeUnit) {
+        Channel channel = APIChannel.get(channelName);
+        if (channel == null) {
+            App.logger.error("Cannot schedule command for channel '" + channelName + "' - channel is null.");
+            return false;
+        }
+        channel.getScheduledCommands().put(command, APIChannel.get(channelName).getScheduler().schedule(task, delay, period, timeUnit));
+        App.logger.debug("Scheduled Command: " + command + " to run every " + period + " " + timeUnit.toString());
+        return true;
     }
 
     private static boolean addToDatabase(String channel, String command, long delay, long period, boolean hourReset, TimeUnit timeUnit) {
