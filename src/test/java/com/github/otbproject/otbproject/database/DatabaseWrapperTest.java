f6369f66e621e976cec3412f1ab19d0469665d59
Justin Wiblin <justin_wiblin@hotmail.co.uk>
2015-05-09 04:01:28 +0100

    More DatabaseWrapper Tests.
    Signed-off-by: Justin Wiblin <justin_wiblin@hotmail.co.uk>

--- src/test/java/com/github/otbproject/otbproject/database/DatabaseWrapperTest.java
+++ src/test/java/com/github/otbproject/otbproject/database/DatabaseWrapperTest.java
@@ -1,5 +1,7 @@
 package com.github.otbproject.otbproject.database;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.LoggerContext;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -10,9 +12,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class DatabaseWrapperTest {
     public static final String path = "target/test.db";
@@ -22,17 +22,32 @@ public class DatabaseWrapperTest {
     public static final HashMap<String, String> fields = new HashMap<>();
     public static DatabaseWrapper db;
     public static final HashMap<String, Object> testData = new HashMap<>();
+    public static final HashMap<String, Object> testDataNew = new HashMap<>();
+    public static final HashMap<String, Object> testDataInt = new HashMap<>();
     public static final String fieldName = "testField";
+    public static final String fieldName2 = "testIntField";
 
 
     @BeforeClass
     public static void initialise() {
-        System.setProperty("OTBCONF", "target/logs");
+        org.apache.logging.log4j.core.Logger coreLogger
+                = (org.apache.logging.log4j.core.Logger) LogManager.getLogger();
+        LoggerContext context
+                = coreLogger.getContext();
+        org.apache.logging.log4j.core.config.Configuration config
+                = context.getConfiguration();
+        coreLogger.removeAppender(config.getAppender("Console-info"));
+        coreLogger.removeAppender(config.getAppender("Console-debug"));
+        coreLogger.removeAppender(config.getAppender("Routing"));
+
         fields.put(fieldName, DataTypes.STRING);
+        fields.put(fieldName2, DataTypes.INTEGER);
         testFields = new TableFields(fields, fieldName);
         tables.put(tableName, testFields);
         db = DatabaseWrapper.createDatabase(path, tables);
         testData.put(fieldName, "Test Data");
+        testDataNew.put(fieldName, "Test Data New");
+        testDataInt.put(fieldName2, 1);
 
     }
     @AfterClass
@@ -40,25 +55,62 @@ public class DatabaseWrapperTest {
         new File(path).delete();
     }
 
-
     @Test
-    public void dataShouldBeInsertedIntoAndRemovedFromDatabase() {
+    public void tableDataShouldBeDumped(){
+        assertNotNull(db.tableDump(tableName));
+    }
+    @Test
+    public void tableDataShouldNotBeDumped(){
+        assertNull(db.tableDump(tableName + "poop"));
+    }
+    @Test
+    public void listOfRecordsShouldBeRetrieved(){
+        assertNotNull(db.getRecordsList(tableName, fieldName));
+    }
+    @Test
+    public void listOfRecordsShouldNotBeRetrieved(){
+        assertNull(db.getRecordsList(tableName+"poop",fieldName));
+    }
+    @Test
+    public void stringDataShouldBeInsertedIntoAndRemovedFromDatabase() {
         assertTrue(db.insertRecord(tableName, testData));
         assertTrue(db.exists(tableName, testData.get(fieldName), fieldName));
         assertTrue(db.removeRecord(tableName, testData.get(fieldName), fieldName));
     }
+
     @Test
-    public void dataShouldBeInsertedIntoAndRetrievedFromDatabase() {
+    public void integerDataShouldBeInsertedIntoAndRemovedFromDatabase(){
+        assertTrue(db.insertRecord(tableName, testDataInt));
+        assertTrue(db.exists(tableName, testDataInt.get(fieldName2), fieldName2));
+        assertTrue(db.removeRecord(tableName, testDataInt.get(fieldName2), fieldName2));
+    }
+
+    @Test
+    public void dataShouldNotBeInsertedIntoDatabase(){
+        assertFalse(db.insertRecord("poop", testData));
+    }
+    @Test
+    public void dataShouldNotBeRemovedFromDatabase(){
+        assertFalse(db.removeRecord(tableName, testData.get(fieldName), fieldName));
+    }
+
+    @Test
+    public void newDataShouldReplaceOldDataInDatabase(){
+        assertTrue(db.insertRecord(tableName, testData));
+        assertTrue(db.exists(tableName, testData.get(fieldName), fieldName));
+        assertTrue(db.updateRecord(tableName, testData.get(fieldName), fieldName, testDataNew));
+        assertFalse(db.exists(tableName, testData.get(fieldName), fieldName));
+        assertTrue(db.exists(tableName, testDataNew.get(fieldName), fieldName));
+        assertTrue(db.removeRecord(tableName, testDataNew.get(fieldName), fieldName));
+    }
+
+    @Test
+    public void dataShouldBeInsertedIntoAndRetrievedFromDatabase() throws SQLException {
         assertTrue(db.insertRecord(tableName, testData));
         assertTrue(db.exists(tableName, testData.get(fieldName), fieldName));
         ResultSet rs = db.getRecord(tableName, testData.get(fieldName), fieldName);
-        try {
-            while(rs.next()){
-                String output = rs.getString(fieldName);
-                assertEquals(output,testData.get(fieldName));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        assertEquals(testData.get(fieldName), rs.getString(fieldName));
+        assertTrue(db.removeRecord(tableName, testData.get(fieldName), fieldName));
+
     }
 }
