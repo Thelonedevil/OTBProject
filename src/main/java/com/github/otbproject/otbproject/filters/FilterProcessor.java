2fe32b44a13b1db5dcde3ea845752b36874b6d2c
NthPortal <mail.vanpelt@gmail.com>
2015-05-09 00:08:29 -0400

    Made plaintext and regex filters match case insensitively.

--- src/main/java/com/github/otbproject/otbproject/filters/FilterProcessor.java
+++ src/main/java/com/github/otbproject/otbproject/filters/FilterProcessor.java
@@ -4,6 +4,7 @@ import com.github.otbproject.otbproject.fs.FSUtil;
 import com.github.otbproject.otbproject.scripts.ScriptProcessor;
 
 import java.io.File;
+import java.util.regex.Pattern;
 
 public class FilterProcessor {
     public static final String METHOD_NAME = "checkMessage";
@@ -16,9 +17,9 @@ public class FilterProcessor {
 
         switch (filter.getType()) {
             case PLAINTEXT:
-                return message.contains(filter.getData());
+                return Pattern.compile(Pattern.quote(filter.getData()), Pattern.CASE_INSENSITIVE).matcher(message).matches();
             case REGEX:
-                return message.matches(filter.getData());
+                return Pattern.compile(filter.getData(), Pattern.CASE_INSENSITIVE).matcher(message).matches();
             case SCRIPT:
                 // TODO possibly tweak method name and parameter(s) passed in
                 return PROCESSOR.process(filter.getData(), (FSUtil.filtersDir() + File.separator + filter.getData()), METHOD_NAME, message);
