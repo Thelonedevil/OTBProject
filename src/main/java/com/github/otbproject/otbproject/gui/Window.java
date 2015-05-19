e5b5b6396a8fcd7e8919658478b8038e4e42fe10
NthPortal <mail.vanpelt@gmail.com>
2015-05-19 17:32:33 -0400

    Fixed up some window stuff.

--- src/main/java/com/github/otbproject/otbproject/gui/Window.java
+++ src/main/java/com/github/otbproject/otbproject/gui/Window.java
@@ -10,7 +10,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 
 public class Window extends JFrame implements ActionListener {
-    private final Window frame = this;
     private final JPanel pnlButtons = new JPanel();
     private final JPanel pnlText = new JPanel();
     private final JButton close = new JButton("Close Window");
@@ -24,19 +23,15 @@ public class Window extends JFrame implements ActionListener {
 
     public Window() {
         super("OTBProject");
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception e) {
-            App.logger.catching(e);
-        }
+
         setSize(400, 280);
         setResizable(false);
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         pnlText.add(text);
         pnlButtons.add(close);
         pnlButtons.add(exit);
-        pnlText.add(pnlButtons);
         getContentPane().add(BorderLayout.CENTER, pnlText);
+        getContentPane().add(BorderLayout.SOUTH, pnlButtons);
         setVisible(true);
         close.addActionListener(this);
         exit.addActionListener(this);
@@ -46,21 +41,9 @@ public class Window extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent event) {
         if (event.getSource().equals(close)) {
-            String confirmMsg = "WARNING: THIS DOES NOT STOP THE BOT.\nClosing this window may make it difficult\nto stop the bot.\nPress \"Cancel\" to keep the window open.";
-            String title = "Confirm Close Without Stopping Bot";
-            if (JOptionPane.showConfirmDialog(this, confirmMsg, title, JOptionPane.OK_CANCEL_OPTION, JOptionPane.WARNING_MESSAGE) == JOptionPane.OK_OPTION) {
-                this.setVisible(false);
-            }
-
+            hideDialog();
         } else if (event.getSource().equals(exit)) {
-            String confirmMsg = "This will stop the bot from running.\nPress \"Cancel\" to continue running the bot.";
-            String title = "Confirm Stop Bot";
-            if (JOptionPane.showConfirmDialog(this, confirmMsg, title, JOptionPane.OK_CANCEL_OPTION, JOptionPane.WARNING_MESSAGE) == JOptionPane.OK_OPTION) {
-                if (APIBot.getBot() != null && APIBot.getBot().isConnected()) {
-                    APIBot.getBot().shutdown();
-                }
-                System.exit(0);
-            }
+            stopDialog();
         }
     }
 
@@ -75,19 +58,32 @@ public class Window extends JFrame implements ActionListener {
             setFocusable(false);
             setOpaque(false);
             setPreferredSize(new Dimension(350, 200));
-            setFont(UIManager.getFont("Label.font"));
+        }
+    }
+
+    private void hideDialog() {
+        String confirmMsg = "WARNING: THIS DOES NOT STOP THE BOT.\nClosing this window may make it difficult\nto stop the bot.\nPress \"Cancel\" to keep the window open.";
+        String title = "Confirm Close Without Stopping Bot";
+        if (JOptionPane.showConfirmDialog(this, confirmMsg, title, JOptionPane.OK_CANCEL_OPTION, JOptionPane.WARNING_MESSAGE) == JOptionPane.OK_OPTION) {
+            this.setVisible(false);
+        }
+    }
+
+    private void stopDialog() {
+        String confirmMsg = "This will stop the bot from running.\nPress \"Cancel\" to continue running the bot.";
+        String title = "Confirm Stop Bot";
+        if (JOptionPane.showConfirmDialog(this, confirmMsg, title, JOptionPane.OK_CANCEL_OPTION, JOptionPane.WARNING_MESSAGE) == JOptionPane.OK_OPTION) {
+            if (APIBot.getBot() != null && APIBot.getBot().isConnected()) {
+                APIBot.getBot().shutdown();
+            }
+            System.exit(0);
         }
     }
 
     private class Closing extends WindowAdapter {
         @Override
         public void windowClosing(java.awt.event.WindowEvent windowEvent) {
-            if (JOptionPane.showConfirmDialog(frame, "This will stop the bot from running", "Are you sure?", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
-                if (APIBot.getBot() != null && APIBot.getBot().isConnected()) {
-                    APIBot.getBot().shutdown();
-                }
-                System.exit(0);
-            }
+            hideDialog();
         }
     }
 }
