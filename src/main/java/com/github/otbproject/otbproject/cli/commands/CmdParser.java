ce77c1e9eeb363fdaca8ae33ca02717e2340a289
NthPortal <mail.vanpelt@gmail.com>
2015-05-25 23:56:10 -0400

    Cleanup after redoing cli commands

--- src/main/java/com/github/otbproject/otbproject/cli/commands/CmdParser.java
+++ src/main/java/com/github/otbproject/otbproject/cli/commands/CmdParser.java
@@ -29,7 +29,6 @@ public class CmdParser {
     public static final String RESTART = "restart";
     public static final String STOP = "stop";
     public static final String HELP = "help";
-    private static final HashMap<String, Runnable> mapOfThings = new HashMap<>();
     private static final HashMap<String, CliCommand> map = new HashMap<>();
     private static final ArrayList<String> args = new ArrayList<>();
     private static String responseStr = "";
@@ -61,12 +60,33 @@ public class CmdParser {
                 .withLongHelp("Clears the specified window(s) or the CLI command history\n"
                         + "Valid targets are: " + ClearTargets.targets.stream().sorted().collect(Collectors.joining(", ")))
                 .withAction(() -> {
-                    App.logger.info("Stopping the process");
-                    if (APIBot.getBot() != null && APIBot.getBot().isConnected()) {
-                        APIBot.getBot().shutdown();
+                    if (args.isEmpty()) {
+                        responseStr = "Not enough args for '" + CLEAR + "'";
+                        return;
                     }
-                    App.logger.info("Process Stopped, Goodbye");
-                    System.exit(0);
+
+                    switch (args.get(0)) {
+                        case ClearTargets.CLI:
+                            GuiApplication.clearCliOutput();
+                            break;
+                        case ClearTargets.LOG:
+                            GuiApplication.clearLog();
+                            break;
+                        case ClearTargets.INFO:
+                            GuiApplication.clearInfo();
+                            break;
+                        case ClearTargets.WINDOWS:
+                            GuiApplication.clearCliOutput();
+                            GuiApplication.clearLog();
+                            GuiApplication.clearInfo();
+                            break;
+                        case ClearTargets.HISTORY:
+                            // TODO make this safe
+                            GuiApplication.clearHistory();
+                            break;
+                        default:
+                            responseStr = "Invalid target to clear: " + args.get(0) + "\nValid targets are: " + ClearTargets.targets.stream().sorted().collect(Collectors.joining(", "));
+                    }
                 });
         map.put(CLEAR, commandBuilder.create());
 
@@ -196,25 +216,16 @@ public class CmdParser {
                     } else {
                         if (map.keySet().contains(args.get(0))) {
                             responseStr = map.get(args.get(0)).getFullHelp();
+                        } else {
+                            printHelpNoCommand();
                         }
                     }
                 });
         map.put(HELP, commandBuilder.create());
-
-
-        //Java 8 magic
-        mapOfThings.put(CLEAR, CmdParser::clear);
-        mapOfThings.put(EXEC, CmdParser::exec);
-        mapOfThings.put(JOINCHANNEL, CmdParser::joinChannel);
-        mapOfThings.put(LEAVECHANNEL, CmdParser::leaveChannel);
-        mapOfThings.put(RELOAD, CmdParser::reload);
-        mapOfThings.put(RESTART, CmdParser::restart);
-        mapOfThings.put(STOP, CmdParser::stop);
-        mapOfThings.put(HELP, CmdParser::help);
     }
 
     public static ImmutableSet<String> getCommands() {
-        return ImmutableSet.copyOf(mapOfThings.keySet());
+        return ImmutableSet.copyOf(map.keySet());
     }
 
     public static void from(String source) {
@@ -235,8 +246,6 @@ public class CmdParser {
                 while (scanner.hasNext()) {
                     args.add(scanner.next());
                 }
-/*                if (mapOfThings.containsKey(name)) {
-                    mapOfThings.get(name).run();*/
                 if (map.containsKey(name)) {
                     map.get(name).run();
                 } else {
@@ -257,187 +266,7 @@ public class CmdParser {
         }
     }
 
-
-    static void stop() {
-        App.logger.info("Stopping the process");
-        if (APIBot.getBot() != null && APIBot.getBot().isConnected()) {
-            APIBot.getBot().shutdown();
-        }
-        App.logger.info("Process Stopped, Goodbye");
-        System.exit(0);
-    }
-
-    static void restart() {
-        if (APIBot.getBot() != null && APIBot.getBot().isConnected()) {
-            APIBot.getBot().shutdown();
-        }
-        FSCommandLoader.LoadCommands();
-        FSCommandLoader.LoadAliases();
-        try {
-            APIBot.setBotThread(new Thread(APIBot.getBotRunnable()));
-            APIBot.getBotThread().start();
-        } catch (IllegalThreadStateException e) {
-            App.logger.catching(e);
-            responseStr = "Restart Failed";
-            return;
-        }
-        responseStr = "Restart Complete";
-    }
-
-    static void joinChannel() {
-        if (args.size() > 0) {
-            boolean success = APIChannel.join(args.get(0).toLowerCase(), true);
-            String string = success ? "Successfully joined" : "Failed to join";
-            responseStr = string + " channel: " + args.get(0).toLowerCase();
-        } else {
-            responseStr = "Not Enough Args for '" + JOINCHANNEL + "'";
-        }
-    }
-
-    static void leaveChannel() {
-        if (args.size() > 0) {
-            boolean success = APIChannel.leave(args.get(0).toLowerCase());
-            String string = success ? "Successfully left" : "Failed to leave";
-            responseStr = string + " channel: " + args.get(0).toLowerCase();
-        } else {
-            responseStr = "Not Enough Args for '" + LEAVECHANNEL + "'";
-        }
-    }
-
-    static void reload() {
-        if (args.size() > 0) {
-            try {
-                FSCommandLoader.LoadLoadedCommands(args.get(0).toLowerCase(), LoadingSet.BOTH);
-            } catch (IOException e) {
-                App.logger.catching(e);
-                responseStr += "Reload of Commands for " + args.get(0).toLowerCase() + " failed. \n " + e.getLocalizedMessage() + "\n";
-            }
-            try {
-                FSCommandLoader.LoadLoadedAliases(args.get(0).toLowerCase(), LoadingSet.BOTH);
-            } catch (IOException e) {
-                App.logger.catching(e);
-                responseStr += "Reload of Aliases for " + args.get(0).toLowerCase() + " failed. \n " + e.getLocalizedMessage() + "\n";
-            }
-        } else {
-            FSCommandLoader.LoadLoadedCommands(LoadingSet.BOTH);
-            FSCommandLoader.LoadLoadedAliases(LoadingSet.BOTH);
-        }
-        responseStr += "Reload Complete";
-    }
-
-    static void exec() {
-        if (args.size() < 2) {
-            responseStr = "Not enough args for '" + EXEC + "'";
-            return;
-        }
-        String channelName = args.get(0).toLowerCase();
-        if (!APIChannel.in(channelName)) {
-            responseStr = "Not in channel: " + channelName;
-            return;
-        }
-        UserLevel ul = UserLevel.INTERNAL;
-        String command = args.get(1);
-        for (int i = 2; i < args.size(); i++) {
-            command += " ";
-            command += args.get(i);
-        }
-        String destinationChannel = InternalMessageSender.DESTINATION_PREFIX + source;
-        PackagedMessage packagedMessage = new PackagedMessage(command, destinationChannel, channelName, destinationChannel, ul, MessagePriority.DEFAULT);
-        try {
-            APIChannel.get(channelName).receiveMessage(packagedMessage);
-            responseStr = "Command output above.";
-        } catch (NullPointerException npe) {
-            App.logger.catching(npe);
-        }
-    }
-
-    static void clear() {
-        if (args.isEmpty()) {
-            responseStr = "Not enough args for '" + CLEAR + "'";
-            return;
-        }
-
-        switch (args.get(0)) {
-            case ClearTargets.CLI:
-                GuiApplication.clearCliOutput();
-                break;
-            case ClearTargets.LOG:
-                GuiApplication.clearLog();
-                break;
-            case ClearTargets.INFO:
-                GuiApplication.clearInfo();
-                break;
-            case ClearTargets.WINDOWS:
-                GuiApplication.clearCliOutput();
-                GuiApplication.clearLog();
-                GuiApplication.clearInfo();
-                break;
-            case ClearTargets.HISTORY:
-                // TODO make this safe
-                GuiApplication.clearHistory();
-                break;
-            default:
-                responseStr = "Invalid target to clear: " + args.get(0) + "\nValid targets are: " + ClearTargets.targets.stream().sorted().collect(Collectors.joining(", "));
-        }
-    }
-
-    public static void help() {
-        if (args.isEmpty()) {
-            responseStr += "clear <target>\n";
-            responseStr += "exec <channel> <command>" + "\n";
-            responseStr += "join <channel>" + "\n";
-            responseStr += "leave <channel>" + "\n";
-            responseStr += "reload <channel>" + "\n";
-            responseStr += "restart" + "\n";
-            responseStr += "stop" + "\n";
-            responseStr += "help <cli command>";
-        } else {
-            switch (args.get(0)) {
-                case CLEAR:
-                    responseStr = "clear <target>\n";
-                    responseStr += "Clears the specified window(s) or the CLI command history\n";
-                    responseStr += "Valid targets are: " + ClearTargets.targets.stream().sorted().collect(Collectors.joining(", "));
-                    break;
-                case EXEC:
-                    responseStr = "exec <channel> <command>" + "\n";
-                    responseStr += "Will run the command denoted by <command> in the channel denoted by <channel>";
-                    break;
-                case JOINCHANNEL:
-                    responseStr = "join <channel>" + "\n";
-                    responseStr += "Will make the bot join the channel denoted by <channel>";
-                    break;
-                case LEAVECHANNEL:
-                    responseStr = "leave <channel>" + "\n";
-                    responseStr += "Will make the bot leave the channel denoted by <channel>";
-                    break;
-                case RELOAD:
-                    responseStr = "reload [channel]" + "\n";
-                    responseStr += "Will reload all commands from the json files. Either for [channel], or if not specified, all channels.";
-                    break;
-                case RESTART:
-                    responseStr = "restart" + "\n";
-                    responseStr += "will restart the bot";
-                    break;
-                case STOP:
-                    responseStr = "stop" + "\n";
-                    responseStr += "Will stop the bot and exit.";
-                    break;
-                case HELP:
-                    responseStr = "help [cli command]" + "\n";
-                    responseStr += "Will print the help message for the cli command denoted by [cli command], or if not specified will list all teh cli commands";
-                    break;
-                default:
-                    printHelpNoCommand();
-                    break;
-            }
-        }
-    }
-
     static void printHelpNoCommand() {
         responseStr = "That command is invalid. \'" + name + "\' does not exist as a CLI command.";
-    }
-
-    void printHelp() {
-        App.logger.info("Invalid arguments.");
     }
 }
