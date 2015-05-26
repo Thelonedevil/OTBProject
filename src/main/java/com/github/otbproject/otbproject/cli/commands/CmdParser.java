a57b33e71ad6687c5e2dc4eafcb12b05b1ed7558
NthPortal <mail.vanpelt@gmail.com>
2015-05-25 23:52:55 -0400

    Redid how the cli commands are created

--- src/main/java/com/github/otbproject/otbproject/cli/commands/CmdParser.java
+++ src/main/java/com/github/otbproject/otbproject/cli/commands/CmdParser.java
@@ -30,6 +30,7 @@ public class CmdParser {
     public static final String STOP = "stop";
     public static final String HELP = "help";
     private static final HashMap<String, Runnable> mapOfThings = new HashMap<>();
+    private static final HashMap<String, CliCommand> map = new HashMap<>();
     private static final ArrayList<String> args = new ArrayList<>();
     private static String responseStr = "";
     private static String name = "RETURN CHARACTER";//Honestly useless assignment, but something has to be here, why not this?
@@ -53,6 +54,154 @@ public class CmdParser {
     }
 
     static {
+        final CliCommand.Builder commandBuilder = new CliCommand.Builder();
+
+        // clear
+        commandBuilder.withShortHelp("clear <target>")
+                .withLongHelp("Clears the specified window(s) or the CLI command history\n"
+                        + "Valid targets are: " + ClearTargets.targets.stream().sorted().collect(Collectors.joining(", ")))
+                .withAction(() -> {
+                    App.logger.info("Stopping the process");
+                    if (APIBot.getBot() != null && APIBot.getBot().isConnected()) {
+                        APIBot.getBot().shutdown();
+                    }
+                    App.logger.info("Process Stopped, Goodbye");
+                    System.exit(0);
+                });
+        map.put(CLEAR, commandBuilder.create());
+
+        // exec
+        commandBuilder.withShortHelp("exec <channel> <command>")
+                .withLongHelp("Will run the command denoted by <command> in the channel denoted by <channel>")
+                .withAction(() -> {
+                    if (args.size() < 2) {
+                        responseStr = "Not enough args for '" + EXEC + "'";
+                        return;
+                    }
+                    String channelName = args.get(0).toLowerCase();
+                    if (!APIChannel.in(channelName)) {
+                        responseStr = "Not in channel: " + channelName;
+                        return;
+                    }
+                    UserLevel ul = UserLevel.INTERNAL;
+                    String command = args.get(1);
+                    for (int i = 2; i < args.size(); i++) {
+                        command += " ";
+                        command += args.get(i);
+                    }
+                    String destinationChannel = InternalMessageSender.DESTINATION_PREFIX + source;
+                    PackagedMessage packagedMessage = new PackagedMessage(command, destinationChannel, channelName, destinationChannel, ul, MessagePriority.DEFAULT);
+                    try {
+                        APIChannel.get(channelName).receiveMessage(packagedMessage);
+                        responseStr = "Command output above.";
+                    } catch (NullPointerException npe) {
+                        App.logger.catching(npe);
+                    }
+                });
+        map.put(EXEC, commandBuilder.create());
+
+        // joinchannel
+        commandBuilder.withShortHelp("join <channel>")
+                .withLongHelp("Will make the bot join the channel denoted by <channel>")
+                .withAction(() -> {
+                    if (args.size() > 0) {
+                        boolean success = APIChannel.join(args.get(0).toLowerCase(), true);
+                        String string = success ? "Successfully joined" : "Failed to join";
+                        responseStr = string + " channel: " + args.get(0).toLowerCase();
+                    } else {
+                        responseStr = "Not Enough Args for '" + JOINCHANNEL + "'";
+                    }
+                });
+        map.put(JOINCHANNEL, commandBuilder.create());
+
+        // leavechannel
+        commandBuilder.withShortHelp("leave <channel>")
+                .withLongHelp("Will make the bot leave the channel denoted by <channel>")
+                .withAction(() -> {
+                    if (args.size() > 0) {
+                        boolean success = APIChannel.leave(args.get(0).toLowerCase());
+                        String string = success ? "Successfully left" : "Failed to leave";
+                        responseStr = string + " channel: " + args.get(0).toLowerCase();
+                    } else {
+                        responseStr = "Not Enough Args for '" + LEAVECHANNEL + "'";
+                    }
+                });
+        map.put(LEAVECHANNEL, commandBuilder.create());
+
+        // reload
+        commandBuilder.withShortHelp("reload <channel>")
+                .withLongHelp("Will reload all commands from the json files. Either for <channel>, or if not specified, all channels.")
+                .withAction(() -> {
+                    if (args.size() > 0) {
+                        try {
+                            FSCommandLoader.LoadLoadedCommands(args.get(0).toLowerCase(), LoadingSet.BOTH);
+                        } catch (IOException e) {
+                            App.logger.catching(e);
+                            responseStr += "Reload of Commands for " + args.get(0).toLowerCase() + " failed. \n " + e.getLocalizedMessage() + "\n";
+                        }
+                        try {
+                            FSCommandLoader.LoadLoadedAliases(args.get(0).toLowerCase(), LoadingSet.BOTH);
+                        } catch (IOException e) {
+                            App.logger.catching(e);
+                            responseStr += "Reload of Aliases for " + args.get(0).toLowerCase() + " failed. \n " + e.getLocalizedMessage() + "\n";
+                        }
+                    } else {
+                        FSCommandLoader.LoadLoadedCommands(LoadingSet.BOTH);
+                        FSCommandLoader.LoadLoadedAliases(LoadingSet.BOTH);
+                    }
+                    responseStr += "Reload Complete";
+                });
+        map.put(RELOAD, commandBuilder.create());
+
+        // restart
+        commandBuilder.withShortHelp("restart")
+                .withLongHelp("will restart the bot")
+                .withAction(() -> {
+                    if (APIBot.getBot() != null && APIBot.getBot().isConnected()) {
+                        APIBot.getBot().shutdown();
+                    }
+                    FSCommandLoader.LoadCommands();
+                    FSCommandLoader.LoadAliases();
+                    try {
+                        APIBot.setBotThread(new Thread(APIBot.getBotRunnable()));
+                        APIBot.getBotThread().start();
+                    } catch (IllegalThreadStateException e) {
+                        App.logger.catching(e);
+                        responseStr = "Restart Failed";
+                        return;
+                    }
+                    responseStr = "Restart Complete";
+                });
+        map.put(RESTART, commandBuilder.create());
+
+        // stop
+        commandBuilder.withShortHelp("stop")
+                .withLongHelp("Will stop the bot and exit.")
+                .withAction(() -> {
+                    App.logger.info("Stopping the process");
+                    if (APIBot.getBot() != null && APIBot.getBot().isConnected()) {
+                        APIBot.getBot().shutdown();
+                    }
+                    App.logger.info("Process Stopped, Goodbye");
+                    System.exit(0);
+                });
+        map.put(STOP, commandBuilder.create());
+
+        // help
+        commandBuilder.withShortHelp("help <cli command>")
+                .withLongHelp("Will print the help message for the cli command denoted by <cli command>, or if not specified will list all the cli commands")
+                .withAction(() -> {
+                    if (args.isEmpty()) {
+                        responseStr = map.keySet().stream().map(key -> map.get(key).getShortHelp()).sorted().collect(Collectors.joining("\n"));
+                    } else {
+                        if (map.keySet().contains(args.get(0))) {
+                            responseStr = map.get(args.get(0)).getFullHelp();
+                        }
+                    }
+                });
+        map.put(HELP, commandBuilder.create());
+
+
         //Java 8 magic
         mapOfThings.put(CLEAR, CmdParser::clear);
         mapOfThings.put(EXEC, CmdParser::exec);
@@ -86,8 +235,10 @@ public class CmdParser {
                 while (scanner.hasNext()) {
                     args.add(scanner.next());
                 }
-                if (mapOfThings.containsKey(name)) {
-                    mapOfThings.get(name).run();
+/*                if (mapOfThings.containsKey(name)) {
+                    mapOfThings.get(name).run();*/
+                if (map.containsKey(name)) {
+                    map.get(name).run();
                 } else {
                     printHelpNoCommand();
                 }
@@ -289,5 +440,4 @@ public class CmdParser {
     void printHelp() {
         App.logger.info("Invalid arguments.");
     }
-
 }
