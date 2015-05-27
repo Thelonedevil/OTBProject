75c9965b3cc47827685d05424a45430e6c54de4b
NthPortal <mail.vanpelt@gmail.com>
2015-05-26 21:11:49 -0400

    Changed how filters work with a BasicFilter for loading from a file or DB, and a Filter for (faster) processing

--- src/main/java/com/github/otbproject/otbproject/filters/Filter.java
+++ src/main/java/com/github/otbproject/otbproject/filters/Filter.java
@@ -1,31 +1,63 @@
 package com.github.otbproject.otbproject.filters;
 
+import com.github.otbproject.otbproject.fs.FSUtil;
+import com.github.otbproject.otbproject.scripts.ScriptProcessor;
+
+import java.io.File;
+import java.util.regex.Pattern;
+
 public class Filter {
-    private String data;
-    private FilterType type;
+    public static final String METHOD_NAME = "checkMessage";
+    private static final ScriptProcessor PROCESSOR = new ScriptProcessor();
+
     private String group;
+    private FilterType type;
+    private Pattern pattern;
+    private String data;
 
-    public String getData() {
-        return data;
-    }
-
-    public void setData(String data) {
-        this.data = data;
-    }
-
-    public FilterType getType() {
-        return type;
-    }
-
-    public void setType(FilterType type) {
+    public Filter(String group, FilterType type, Pattern pattern, String data) {
+        this.group = group;
         this.type = type;
+        this.pattern = pattern;
+        this.data = data; // Script if script; original regex string otherwise
     }
 
     public String getGroup() {
         return group;
     }
 
-    public void setGroup(String group) {
-        this.group = group;
+    public boolean matches(String message) {
+        switch (type) {
+            case PLAINTEXT:
+            case REGEX:
+                return pattern.matcher(message).matches();
+            case SCRIPT:
+                // TODO possibly tweak method name and parameter(s) passed in
+                return PROCESSOR.process(data, (FSUtil.filtersDir() + File.separator + data), METHOD_NAME, message);
+            // Default should never occur
+            default:
+                return false;
+        }
+    }
+
+    public BasicFilter toBasicFilter() {
+        BasicFilter filter = new BasicFilter();
+        filter.setGroup(group);
+        filter.setType(type);
+        filter.setData(data);
+        return filter;
+    }
+
+    public static Filter fromBasicFilter(BasicFilter filter) {
+        Pattern pattern = null;
+        switch (filter.getType()) {
+            case PLAINTEXT:
+                pattern = Pattern.compile(Pattern.quote(filter.getData()), Pattern.CASE_INSENSITIVE);
+                break;
+            case REGEX:
+                pattern = Pattern.compile(filter.getData(), Pattern.CASE_INSENSITIVE);
+                break;
+        }
+        return new Filter(filter.getGroup(), filter.getType(), pattern, filter.getData());
     }
 }
