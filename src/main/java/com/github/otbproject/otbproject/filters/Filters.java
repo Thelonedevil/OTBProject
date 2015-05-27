85cf40b742e009bae4f78b6fd9b8474145d0fec9
NthPortal <mail.vanpelt@gmail.com>
2015-05-27 05:39:01 -0400

    Added database api for filters

---
+++ src/main/java/com/github/otbproject/otbproject/filters/Filters.java
@@ -0,0 +1,98 @@
+package com.github.otbproject.otbproject.filters;
+
+import com.github.otbproject.otbproject.App;
+import com.github.otbproject.otbproject.database.DatabaseWrapper;
+import com.github.otbproject.otbproject.util.CustomCollectors;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Filters {
+    public static BasicFilter get(DatabaseWrapper db, String data) {
+        if (db.exists(FilterFields.TABLE_NAME, data, FilterFields.DATA)) {
+            ResultSet rs = db.getRecord(FilterFields.TABLE_NAME, data, FilterFields.DATA);
+            try {
+                return getFilterFromResultSet(rs);
+            } catch (SQLException e) {
+                App.logger.catching(e);
+            } finally {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    App.logger.catching(e);
+                }
+            }
+        }
+        return null;
+    }
+
+    public static ArrayList<BasicFilter> getBasicFilters(DatabaseWrapper db) {
+        ArrayList<BasicFilter> filters = new ArrayList<>();
+        ResultSet rs = db.tableDump(FilterFields.TABLE_NAME);
+        try {
+            while (rs.next()) {
+                filters.add(getFilterFromResultSet(rs));
+            }
+        } catch (SQLException e) {
+            App.logger.catching(e);
+        } finally {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                App.logger.catching(e);
+            }
+        }
+        return filters;
+    }
+
+    public static ArrayList<Filter> getAllFilters(DatabaseWrapper db) {
+        return getBasicFilters(db).stream().map(Filter::fromBasicFilter).collect(CustomCollectors.toArrayList());
+    }
+
+    // TODO possibly remove
+    public static ArrayList<String> getFilterDataOfType(DatabaseWrapper db, FilterType type) {
+        return getBasicFilters(db).stream().filter(basicFilter -> (basicFilter.getType() == type)).map(BasicFilter::getData).collect(CustomCollectors.toArrayList());
+    }
+
+    private static BasicFilter getFilterFromResultSet(ResultSet rs) throws SQLException {
+        BasicFilter basicFilter = new BasicFilter();
+        basicFilter.setData(rs.getString(FilterFields.DATA));
+        basicFilter.setType(FilterType.valueOf(rs.getString(FilterFields.TYPE)));
+        basicFilter.setGroup(rs.getString(FilterFields.GROUP));
+        basicFilter.setEnabled(Boolean.valueOf(rs.getString(FilterFields.ENABLED)));
+        return basicFilter;
+    }
+
+    public static boolean update(DatabaseWrapper db, HashMap<String, Object> map) {
+        return db.updateRecord(FilterFields.TABLE_NAME, map.get(FilterFields.DATA), FilterFields.DATA, map);
+    }
+
+    public static boolean exists(DatabaseWrapper db, String commandName) {
+        return db.exists(FilterFields.TABLE_NAME, commandName, FilterFields.DATA);
+    }
+
+    public static boolean add(DatabaseWrapper db, HashMap<String, Object> map) {
+        return db.insertRecord(FilterFields.TABLE_NAME, map);
+    }
+
+    public static boolean remove(DatabaseWrapper db, String data) {
+        return db.removeRecord(FilterFields.TABLE_NAME, data, FilterFields.DATA);
+    }
+
+    public static boolean addFilterFromObj(DatabaseWrapper db, BasicFilter basicFilter) {
+        HashMap<String, Object> map = new HashMap<>();
+
+        map.put(FilterFields.DATA, basicFilter.getData());
+        map.put(FilterFields.TYPE, basicFilter.getType().name());
+        map.put(FilterFields.GROUP, basicFilter.getGroup());
+        map.put(FilterFields.ENABLED, basicFilter.isEnabled().toString());
+
+        if (exists(db, basicFilter.getData())) {
+            return update(db, map);
+        } else {
+            return add(db, map);
+        }
+    }
+}
