c8ad4cf78804826b8da6d3ee549b2d9a0199ff56
NthPortal <mail.vanpelt@gmail.com>
2015-05-28 22:14:30 -0400

    Wrote cache and cache lookup for Beam messages using HashMaps for fast lookup

--- src/main/java/com/github/otbproject/otbproject/beam/BeamChatChannel.java
+++ src/main/java/com/github/otbproject/otbproject/beam/BeamChatChannel.java
@@ -3,16 +3,25 @@ package com.github.otbproject.otbproject.beam;
 import com.github.otbproject.otbproject.App;
 import com.github.otbproject.otbproject.api.APIBot;
 import com.github.otbproject.otbproject.proc.CooldownSet;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.RemovalListener;
+import com.google.common.cache.RemovalNotification;
 import pro.beam.api.resource.BeamUser;
 import pro.beam.api.resource.channel.BeamChannel;
 import pro.beam.api.resource.chat.BeamChat;
 import pro.beam.api.resource.chat.BeamChatConnectable;
 import pro.beam.api.resource.chat.events.IncomingMessageEvent;
+import pro.beam.api.resource.chat.events.data.IncomingMessageData;
 import pro.beam.api.resource.chat.methods.AuthenticateMessage;
 import pro.beam.api.services.impl.ChatService;
 import pro.beam.api.services.impl.UsersService;
 
+import java.util.Collections;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 public class BeamChatChannel {
     final BeamBot beamBot;
@@ -21,8 +30,26 @@ public class BeamChatChannel {
     BeamChannel channel;
     public final CooldownSet<String> timeoutSet = new CooldownSet<>();
 
+    public static final int CACHE_EXPIRATION_MIN = 5;
+    public final ConcurrentHashMap<String, Set<String>> cacheLookup = new ConcurrentHashMap<>();
+    public final Cache<String, IncomingMessageData> messageCache;
+
 
     public BeamChatChannel(String channelName){
+        // Initialize cache
+        messageCache = CacheBuilder.newBuilder()
+                .expireAfterWrite(CACHE_EXPIRATION_MIN, TimeUnit.MINUTES)
+                .removalListener(new RemovalListener<String, IncomingMessageData>() {
+                    @Override
+                    public void onRemoval(RemovalNotification<String, IncomingMessageData> notification) {
+                        IncomingMessageData data = notification.getValue();
+                        if (data != null) {
+                            removeFromCacheLookup(notification.getValue().user_name, notification.getKey());
+                        }
+                    }
+                })
+                .build();
+
         beamBot = ((BeamBot) APIBot.getBot());
         try {
             channel = beamBot.beamUser.channel;
@@ -54,5 +81,46 @@ public class BeamChatChannel {
             App.logger.info("Connected to: " + channelName);
 
         }
+    }
+
+    // TODO maybe lock
+    private void addToCacheLookup(String user, String messageId) {
+        Set<String> set = cacheLookup.get(user);
+        if (set != null) {
+            set.add(messageId);
+        } else {
+            Set<String> newSet = Collections.newSetFromMap(new ConcurrentHashMap<>());
+            newSet.add(messageId);
+            set = cacheLookup.put(user, newSet);
+            if (set != null) {
+                newSet.addAll(set);
+            }
+        }
+    }
+
+    // TODO maybe lock
+    private boolean removeFromCacheLookup(String user, String messageId) {
+        Set<String> set = cacheLookup.get(user);
+        if (set != null) {
+            boolean success = set.remove(messageId);
+            if (set.isEmpty()) {
+                cacheLookup.remove(user);
+            }
+            return success;
+        }
+        return false;
+    }
+
+    public void cacheMessage(IncomingMessageData data) {
+        if (data == null) {
+            return;
+        }
+        addToCacheLookup(data.user_name, data.id);
+        messageCache.put(data.id, data);
+    }
+
+    public void clearCache() {
+        messageCache.invalidateAll();
+        cacheLookup.clear();
     }
 }
