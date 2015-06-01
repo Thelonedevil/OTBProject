53f803ab701ffb43b1737f3fe5e2c49a20055804
NthPortal <mail.vanpelt@gmail.com>
2015-05-31 20:10:32 -0400

    Started working on preload comparators

--- src/main/java/com/github/otbproject/otbproject/util/unpack/PreloadComparator.java
+++ src/main/java/com/github/otbproject/otbproject/util/unpack/PreloadComparator.java
@@ -8,18 +8,26 @@ import com.github.otbproject.otbproject.filters.FilterGroup;
 
 class PreloadComparator {
     static LoadedAlias generateAliasHybrid(DatabaseWrapper db, LoadedAlias newAlias, LoadedAlias oldAlias) {
-
+        if ((oldAlias == null) || !oldAlias.getName().equals(newAlias.getName())) {
+            return newAlias;
+        }
     }
 
     static LoadedCommand generateCommandHybrid(DatabaseWrapper db, LoadedCommand newCommand, LoadedCommand oldCommand) {
-
+        if ((oldCommand == null) || !oldCommand.getName().equals(newCommand.getName())) {
+            return newCommand;
+        }
     }
 
     static BasicFilter generateFilterHybrid(DatabaseWrapper db, BasicFilter newFilter, BasicFilter oldFilter) {
-
+        if ((oldFilter == null) || !(oldFilter.getData().equals(newFilter.getData()) && oldFilter.getType().equals(newFilter.getType()))) {
+            return newFilter;
+        }
     }
 
     static FilterGroup generateFilterGroupHybrid(DatabaseWrapper db, FilterGroup newGroup, FilterGroup oldGroup) {
-
+        if ((oldGroup == null) || !oldGroup.getName().equals(newGroup.getName())) {
+            return newGroup;
+        }
     }
 }
