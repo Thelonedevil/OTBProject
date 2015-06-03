2b9a71d9901f2f3333e3e2c5acab88133ae3f8f3
NthPortal <mail.vanpelt@gmail.com>
2015-06-02 20:42:17 -0400

    Redid ChannelMessageSender. Cleaned up Channel a bit as a result

--- src/main/java/com/github/otbproject/otbproject/channels/Channel.java
+++ src/main/java/com/github/otbproject/otbproject/channels/Channel.java
@@ -5,9 +5,6 @@ import com.github.otbproject.otbproject.commands.scheduler.Scheduler;
 import com.github.otbproject.otbproject.config.ChannelConfig;
 import com.github.otbproject.otbproject.database.DatabaseWrapper;
 import com.github.otbproject.otbproject.database.SQLiteQuoteWrapper;
-import com.github.otbproject.otbproject.filters.FilterGroups;
-import com.github.otbproject.otbproject.filters.FilterManager;
-import com.github.otbproject.otbproject.filters.Filters;
 import com.github.otbproject.otbproject.messages.receive.ChannelMessageReceiver;
 import com.github.otbproject.otbproject.messages.receive.MessageReceiveQueue;
 import com.github.otbproject.otbproject.messages.receive.PackagedMessage;
@@ -25,7 +22,6 @@ import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class Channel {
-    private final MessageSendQueue sendQueue = new MessageSendQueue(this);
     private final MessageReceiveQueue receiveQueue = new MessageReceiveQueue();
     private final CooldownSet<String> commandCooldownSet = new CooldownSet<>();
     private final CooldownSet<String> userCooldownSet = new CooldownSet<>();
@@ -35,7 +31,6 @@ public class Channel {
     private final DatabaseWrapper mainDb;
     private final SQLiteQuoteWrapper quoteDb;
     private ChannelMessageSender messageSender;
-    private Thread messageSenderThread;
     private ChannelMessageReceiver messageReceiver;
     private Thread messageReceiverThread;
     private final Scheduler scheduler = new Scheduler();
@@ -70,9 +65,8 @@ public class Channel {
                 return false;
             }
 
-            messageSender = new ChannelMessageSender(this, sendQueue);
-            messageSenderThread = new Thread(messageSender);
-            messageSenderThread.start();
+            messageSender = new ChannelMessageSender(this);
+            messageSender.start();
 
             messageReceiver = new ChannelMessageReceiver(this, receiveQueue);
             messageReceiverThread = new Thread(messageReceiver);
@@ -96,10 +90,8 @@ public class Channel {
             }
             inChannel = false;
 
-            messageSenderThread.interrupt();
-            messageSenderThread = null;
+            messageSender.stop();
             messageSender = null;
-            sendQueue.clear();
 
             messageReceiverThread.interrupt();
             messageReceiverThread = null;
@@ -121,7 +113,7 @@ public class Channel {
     public boolean sendMessage(MessageOut messageOut) {
         lock.readLock().lock();
         try {
-            return inChannel && sendQueue.add(messageOut);
+            return inChannel && messageSender.send(messageOut);
         } finally {
             lock.readLock().unlock();
         }
