c9cf9b8273217ea9e8a7473b9e0be8b9abeede97
NthPortal <mail.vanpelt@gmail.com>
2015-06-07 21:38:28 -0400

    Suppressed unchecked warning and cleaned up for loop in UnPacker

--- src/main/java/com/github/otbproject/otbproject/util/UnPacker.java
+++ src/main/java/com/github/otbproject/otbproject/util/UnPacker.java
@@ -5,8 +5,7 @@ import net.lingala.zip4j.core.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
 import net.lingala.zip4j.model.FileHeader;
 
-
-import java.io.*;
+import java.io.File;
 import java.net.URISyntaxException;
 import java.util.List;
 
@@ -15,13 +14,12 @@ public class UnPacker {
     public static void unPack(String dir, String destDir){
         try {
             ZipFile jar = new ZipFile(new File(App.class.getProtectionDomain().getCodeSource().getLocation().toURI()));
+            @SuppressWarnings("unchecked")
             List<FileHeader> fileHeaders = jar.getFileHeaders();
             for (FileHeader fileHeader : fileHeaders){
-                if(fileHeader.getFileName().startsWith(dir)) {
-                    if(!fileHeader.isDirectory()) {
-                        App.logger.debug(fileHeader.getFileName());
-                        jar.extractFile(fileHeader, destDir, null, fileHeader.getFileName().substring(dir.length()));
-                    }
+                if(fileHeader.getFileName().startsWith(dir) && !fileHeader.isDirectory()) {
+                    App.logger.debug(fileHeader.getFileName());
+                    jar.extractFile(fileHeader, destDir, null, fileHeader.getFileName().substring(dir.length()));
                 }
             }
         } catch (URISyntaxException | ZipException e) {
