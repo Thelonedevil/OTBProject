90edb947082e7221e1d0e1cf1ca3fa55730b6ae8
NthPortal <mail.vanpelt@gmail.com>
2015-07-12 15:59:19 -0400

    Rewrote preload unpacking and removed zip4j.

---
+++ src/main/java/com/github/otbproject/otbproject/util/Unpacker.java
@@ -0,0 +1,33 @@
+package com.github.otbproject.otbproject.util;
+
+import com.github.otbproject.otbproject.App;
+import org.apache.commons.io.IOUtils;
+
+import java.io.*;
+import java.net.URISyntaxException;
+import java.util.zip.ZipFile;
+
+public class Unpacker {
+    public static void unpack(String jarDir, String destDir) {
+        try {
+            ZipFile jar = new ZipFile(new File(App.class.getProtectionDomain().getCodeSource().getLocation().toURI()));
+            jar.stream()
+                    .filter(zipEntry -> zipEntry.getName().startsWith(jarDir) && !zipEntry.isDirectory())
+                    .forEach(zipEntry -> {
+                        try {
+                            App.logger.debug("Unpacking: " + zipEntry.getName());
+                            String fileName = zipEntry.getName().substring(jarDir.length());
+                            InputStream in = jar.getInputStream(zipEntry);
+                            OutputStream out = new FileOutputStream(destDir + File.separator + fileName);
+                            IOUtils.copy(in, out);
+                            IOUtils.closeQuietly(in);
+                            out.close();
+                        } catch (IOException e) {
+                            App.logger.catching(e);
+                        }
+                    });
+        } catch (URISyntaxException | IOException e) {
+            App.logger.catching(e);
+        }
+    }
+}
