9bb18b58cc2825306d50919720e7771897f5e304
NthPortal <mail.vanpelt@gmail.com>
2015-07-13 15:30:08 -0400

    Moved some stuff around and added method to get latest app version from github.

--- src/main/java/com/github/otbproject/otbproject/util/version/AppVersion.java
+++ src/main/java/com/github/otbproject/otbproject/util/version/AppVersion.java
@@ -1,13 +1,13 @@
 package com.github.otbproject.otbproject.util.version;
 
-
 public class AppVersion {
+    private static Version LATEST;
 
     public String getVersionString() {
         return getClass().getPackage().getImplementationVersion();
     }
 
-    public Version getVersion() {
+    public Version getCurrentVersion() {
         Version version;
         try {
             version = Version.parseVersion(getVersionString());
@@ -16,5 +16,19 @@ public class AppVersion {
             version = Version.create(0, 0, 0, Version.Type.RELEASE);
         }
         return version;
+    }
+
+    public Version latest() {
+        if (LATEST == null) {
+            getLatest();
+        }
+        return LATEST;
+    }
+
+    private synchronized void getLatest() {
+        if (LATEST == null) {
+            LATEST = Version.parseAsOptional(Versions.lookupLatestGithubVersion("otbproject"))
+                    .orElse(Version.create(0, 0, 0, Version.Type.RELEASE));
+        }
     }
 }
