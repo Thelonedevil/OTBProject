033231e5f70d4d6fa50638ccaedfb35c14b14dfb
NthPortal <mail.vanpelt@gmail.com>
2015-08-16 03:36:06 -0400

    Replaced set of message handlers in Abstract bot with a single Consumer, and used Consumer::andThen to add new handlers.

--- src/main/java/com/github/otbproject/otbproject/bot/AbstractBot.java
+++ src/main/java/com/github/otbproject/otbproject/bot/AbstractBot.java
@@ -5,14 +5,13 @@ import com.github.otbproject.otbproject.database.DatabaseWrapper;
 import com.github.otbproject.otbproject.database.Databases;
 import com.github.otbproject.otbproject.messages.receive.PackagedMessage;
 
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Consumer;
 
 public abstract class AbstractBot implements IBot {
     protected final ConcurrentHashMap<String, Channel> channels = new ConcurrentHashMap<>();
     protected final DatabaseWrapper botDB = Databases.createBotDbWrapper();
-    protected final Set<Consumer<PackagedMessage>> messageListeners = ConcurrentHashMap.newKeySet();
+    protected Consumer<PackagedMessage> messageHandlers = packagedMessage -> {};
 
     @Override
     public ConcurrentHashMap<String, Channel> getChannels() {
@@ -26,11 +25,11 @@ public abstract class AbstractBot implements IBot {
 
     @Override
     public void onMessage(Consumer<PackagedMessage> messageHandler) {
-        messageListeners.add(messageHandler);
+        messageHandlers = messageHandlers.andThen(messageHandler);
     }
 
     @Override
     public void invokeMessageHandlers(PackagedMessage message) {
-        messageListeners.forEach(consumer -> consumer.accept(message));
+        messageHandlers.accept(message);
     }
 }
